e3bb39132ff55244231cd4aa61fce68377f08ddd
Boon-Chung Chi <boon.chung.chi@gmx.at>
2025-02-13 14:57:13 +0100

    Updates

--- src/components/utils/Button.jsx
+++
@@ -1,9 +0,0 @@
-import "./utils.css"
-
-const Button = ({ className, link, text }) => {
-    return (
-        <a className={className} type="button" href={link}>{text}</a>
-    );
-}
-
-export default Button;
