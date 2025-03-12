d3ef1013599f99de3ef70eb7037e932b6a1f7d7f
Boon-Chung Chi <boon.chung.chi@gmx.at>
2025-03-12 21:37:23 +0100

    removed unused components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Content from "./components/content/Content";
 import Footer from "./components/footer/Footer";
 import About from "./components/about/About";
 import VideoSection from "./components/videosection/VideoSection";
-import Carousel from "./components/carousel/Carousel";
-import Team from "./components/team/Team";
 import ImageSection from "./components/images/ImageSection";
 import Banner from "./components/banner/Banner";
 import Problems from "./components/problems/Problems";
